7d4af4ff9e2b9d3c534f927cb8ed447a02d91fa5
XLED-bd <korol.gorox@gmail.com>
2023-10-25 13:31:30 +0300

    test

--- app/src/main/java/com/example/rksi/MainActivity.java
+++ app/src/main/java/com/example/rksi/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.rksi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ScrollView scrollView = (ScrollView) findViewById(R.id.scroll);
+
+        for (int i = 0; i<10; i++) {
+            TextView textView = new TextView(this);
+            textView.setText(i);
+            textView.setTextSize(26);
+            scrollView.addView(textView);
+        }
         /* TextView text = (TextView)findViewById(R.id.text);
 
         String str = new String();
