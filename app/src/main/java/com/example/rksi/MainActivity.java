d05ef942541df4b13ca5d3824de247a4d64a8fb6
MaxyUMus <pilipenkomaksim57@gmail.com>
2023-10-25 14:31:42 +0300

    adaptive_list

--- app/src/main/java/com/example/rksi/MainActivity.java
+++ app/src/main/java/com/example/rksi/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.rksi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
-                |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -31,15 +36,23 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE;
         decorView.setSystemUiVisibility(uiOptions);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+
+        RelativeLayout layout = findViewById(R.id.rectangle_1);
 
         ScrollView scrollView = (ScrollView) findViewById(R.id.scroll);
         LinearLayout lay = new LinearLayout(this);
         lay.setOrientation(LinearLayout.VERTICAL);
         for (int i = 0; i < 50; i++){
             TextView view = new TextView(this);
-            view.setText("test" + i);
+            view.setText(""+i);
             view.setTextSize(15f);
-            lay.addView(view, 300, 100);
+            lay.addView(view, width, 100);
         }
         scrollView.addView(lay);
     }
