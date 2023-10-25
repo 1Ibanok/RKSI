4bdf005a3b67d4d9372181da3d01e71606f2aaaf
MaxyUMus <pilipenkomaksim57@gmail.com>
2023-10-25 21:14:47 +0300

    code fixed 2

--- app/src/main/java/com/example/rksi/Job.java
+++ app/src/main/java/com/example/rksi/Job.java
@@ -12,6 +12,20 @@ public class Job extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.job);
 
+        View decorView = getWindow().getDecorView(); //скрыть панель навигации
+
+        //Скрываем "чёлку"
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(uiOptions);
 
     }
 
