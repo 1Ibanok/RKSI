7032261970a4a584f7cd4df1c204bd3d08d0c571
MaxyUMus <pilipenkomaksim57@gmail.com>
2023-10-26 11:08:59 +0300

    database

--- app/src/main/java/com/example/rksi/MainActivity.java
+++ app/src/main/java/com/example/rksi/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         View decorView = getWindow().getDecorView(); //скрыть панель навигации
 
         //Скрываем "чёлку"
