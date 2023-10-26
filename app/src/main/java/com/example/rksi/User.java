dce760a1b6754fdb6df78f62f3294af0ab15c8ea
XLED-bd <korol.gorox@gmail.com>
2023-10-26 13:13:33 +0300

    Регистрация бэкенд

---
+++ app/src/main/java/com/example/rksi/User.java
@@ -0,0 +1,13 @@
+package com.example.rksi;
+
+public class User {
+    public String first_name, second_name, email, phone, password;
+
+    public User(String firstName, String secondName, String email, String phone, String password) {
+        this.first_name = firstName;
+        this.second_name = secondName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+}
--- app/src/main/java/com/example/rksi/hello_activity.java
+++ app/src/main/java/com/example/rksi/hello_activity.java
@@ -5,13 +5,28 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class hello_activity extends AppCompatActivity {
+
+    private String USER_KEY = "User";
+    public DatabaseReference UsersBD;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +47,69 @@ public class hello_activity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE;
         decorView.setSystemUiVisibility(uiOptions);
+    }
 
+    public ArrayList<User> GetAllUsers(View view){
+        UsersBD = FirebaseDatabase.getInstance("https://rksi-2e196-default-rtdb.europe-west1.firebasedatabase.app/").getReference(USER_KEY);
+        ArrayList<User> Users = new ArrayList<>();
+        ValueEventListener vListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    User user = ds.getValue(User.class);
+                    Users.add(user);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Все плохо
+            }
+
+        };
+    }
+
+    public void Reg(View view){
+        EditText First_name = findViewById(R.id.first_name);
+        EditText Second_name = findViewById(R.id.second_name);
+        EditText Email = findViewById(R.id.email);
+        EditText Phone = findViewById(R.id.phone);
+        EditText Password = findViewById(R.id.password);
+
+        UsersBD = FirebaseDatabase.getInstance("https://rksi-2e196-default-rtdb.europe-west1.firebasedatabase.app/").getReference(USER_KEY);
+
+        String first_name = First_name.getText().toString();
+        String second_name = Second_name.getText().toString();
+        String email = Email.getText().toString();
+        String phone = Phone.getText().toString();
+        String password = Password.getText().toString();
+        User newUser = new User(first_name, second_name, email, phone, password);
+
+        UsersBD.push().setValue(newUser);
+        UsersBD.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Сделать уведомлние что все хорошо
+
+                GridLayout registration = findViewById(R.id.registration);
+                registration.setVisibility(GridLayout.INVISIBLE);
+
+                GridLayout login = findViewById(R.id.login);
+                login.setVisibility(GridLayout.VISIBLE);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Сделать уведомлние что все плохо
+            }
+        });
+    }
+
+    public void Log(){
 
     }
 
-    public void Login(View view) {
+    public void toLogin(View view) {
         GridLayout registration = findViewById(R.id.registration);
         registration.setVisibility(GridLayout.INVISIBLE);
 
@@ -44,7 +117,7 @@ public class hello_activity extends AppCompatActivity {
         login.setVisibility(GridLayout.VISIBLE);
     }
 
-    public void Registration(View view) {
+    public void toRegistration(View view) {
         GridLayout login = findViewById(R.id.login);
         login.setVisibility(GridLayout.INVISIBLE);
 
