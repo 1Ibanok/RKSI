e9ce7249c414b0b92307f75af80b2e1264612bbd
XLED-bd <korol.gorox@gmail.com>
2023-10-26 16:25:15 +0300

    123

--- app/src/main/java/com/example/rksi/MainActivity.java
+++ app/src/main/java/com/example/rksi/MainActivity.java
@@ -12,14 +12,26 @@ import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private int width;
     private int height;
+
+    private DatabaseReference TiketsBD;
     User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +68,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Refresh(){
+        TiketsBD = FirebaseDatabase.getInstance().getReference("TIKETS");
+        ArrayList<Tiket> ListTikets = new ArrayList<>();
+
         //Находим окно прокрутки
         ScrollView scrollView = findViewById(R.id.jobs_scroll);
 
@@ -65,6 +80,20 @@ public class MainActivity extends AppCompatActivity {
         //Задаём то, что контент сортируется по вертикали
         lay.setOrientation(GridLayout.VERTICAL);
         lay.removeAllViews();
+
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    Tiket tiket = ds.getValue(Tiket.class);
+                    ListTikets.add(tiket);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        };
+
         for (int i = 0; i < 60; i++){
 
             //Создаём переменную отвечающую за индекс кнопки
